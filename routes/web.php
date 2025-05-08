4b9d088510d57877c7908a828b78f950e91143b7
bodzzzzz <sim.abdelrahmanmoamen92438@alexu.edu.eg>
2025-05-08 03:04:52 +0300

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,12 @@
 
 use Illuminate\Support\Facades\Route;
 
+// Default welcome route
 Route::get('/', function () {
     return view('welcome');
+});
+
+// Health check route (matches your bootstrap config)
+Route::get('/up', function () {
+    return 'OK';
 });
